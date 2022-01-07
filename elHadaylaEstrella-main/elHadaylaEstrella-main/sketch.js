ca40c01281bd5bdb91e4c5671553673de4514f50
marcopablopulido <93391197+marcopablopulido@users.noreply.github.com>
2022-01-07 11:52:51 -0600

    Add files via upload

---
+++ elHadaylaEstrella-main/elHadaylaEstrella-main/sketch.js
@@ -0,0 +1,66 @@
+var starImg,bgImg;
+var star, starBody;
+//crea la variable para el sprite del hada y fairyImg
+var hada;
+var fairyImg;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	//carga aquí la animación del hada
+}   hada = loadImage("images/fairy.png");
+
+function setup() {
+	createCanvas(800, 750);
+
+
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  //escribe aquí el código para detener la estrella en la mano del hada
+if(star.y>470 && starBody.position.y >470) {
+	Matter.Body.setStatic(starBody,true);
+}
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//escribe el código para mover al hada a la izquierda y derecha
+	if (keyCode === RIGTH_ARROW){
+		hada.x=4,hada.y=0;
+
+	}
+}
